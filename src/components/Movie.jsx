3e90cfdfbc91abf519565d27b99c8d5b04d69188
Omar Al-Azzawi <alazzawiomar01@gmail.com>
2021-07-04 22:12:39 +0300

    add more style

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -2,10 +2,17 @@ import React from 'react'
 
 const IMG_API = 'https://image.tmdb.org/t/p/w1280';
 
-function Movie({ title, poster_path}) {
+function Movie({ title, poster_path, vote_average, overview}) {
     return (
         <div className="movie">
            <img src={IMG_API + poster_path} alt={title} />
+           <div className="movie_info">
+               <h3>{title}</h3>
+               <span>{vote_average}</span>
+           </div>
+           <div className="movie_overview">
+           <p>{overview}</p>
+           </div>
         </div>
     )
 }
