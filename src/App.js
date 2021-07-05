bbef475f594798c47f88df905409d16cbe82f686
Omar Al-Azzawi <alazzawiomar01@gmail.com>
2021-07-05 21:51:03 +0300

    add more style

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import { useState, useEffect } from "react";
 import './App.css';
 import Movie from "./components/Movie"
+import { GiPopcorn } from "react-icons/gi";
 
-const API = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=5160ae527d1c323f1979fb60a2c3cacb';
-const IMG_API = 'https://image.tmdb.org/t/p/w1280';
-const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=5160ae527d1c323f1979fb60a2c3cacb';
+const API = 'https://api.themoviedb.org/3/trending/movie/day?api_key=5160ae527d1c323f1979fb60a2c3cacb';
+const SEARCH_API = 'https://api.themoviedb.org/3/search/multi?api_key=5160ae527d1c323f1979fb60a2c3cacb';
 
 function App() {
 const[movies, setMovies] = useState([]);
+const[searchTerm, setSearchTerm] = useState('');
 
 
 
@@ -16,16 +17,37 @@ useEffect(() =>{
   .then(res => res.json())
   .then(data => {
     setMovies(data.results);
-    console.log(data);
   });
 },[]);
 
+const handleOnSubmit = (event) =>{
+  event.preventDefault();
+
+  fetch(SEARCH_API + searchTerm)
+  .then(res => res.json())
+  .then(data => {
+    setMovies(data.results);
+  });
+}
+
+const handleOnChange = (e) =>{
+  setSearchTerm(e.target.value);
+};
+
   return (
-    <div className="container">
-      {movies.map((movie) =>(
-        <Movie key={movie.id} {...movie}/>
-      ))}
-    </div>
+    <>
+      <header className="header">
+        <h1><GiPopcorn />Movie Night</h1>
+        <form onSubmit={handleOnSubmit}>
+          <input className="header_search" type="search" placeholder="Search..." value={searchTerm} onChange={handleOnChange}/>
+        </form>
+      </header>
+      <div className="container">
+        {movies && movies.map((movie) =>(
+          <Movie key={movie.id} {...movie}/>
+        ))}
+      </div>
+    </>
   );
 }
 
