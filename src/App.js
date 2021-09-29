fe0b4bd5fc104c05c826b2c2f7689f9cbc67b978
Omar Al-Azzawi <alazzawiomar01@gmail.com>
2021-09-29 16:37:57 +0300

    fix some issues

--- src/App.js
+++ src/App.js
@@ -1,51 +1,56 @@
 import { useState, useEffect } from "react";
-import './App.css';
-import Movie from "./components/Movie"
+import "./App.css";
+import Movie from "./components/Movie";
 import { GiPopcorn } from "react-icons/gi";
 
-const API = 'https://api.themoviedb.org/3/trending/movie/day?api_key=5160ae527d1c323f1979fb60a2c3cacb';
-const SEARCH_API = 'https://api.themoviedb.org/3/search/multi?api_key=5160ae527d1c323f1979fb60a2c3cacb';
+const API = `https://api.themoviedb.org/3/trending/movie/day?api_key=${process.env.REACT_APP_API_KEY}`;
+const SEARCH_API = `https://api.themoviedb.org/3/search/multi?api_key=${process.env.REACT_APP_API_KEY}`;
 
 function App() {
-const[movies, setMovies] = useState([]);
-const[searchTerm, setSearchTerm] = useState('');
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
 
+  useEffect(() => {
+    fetch(API)
+      .then((res) => res.json())
+      .then((data) => {
+        setMovies(data.results);
+      });
+  }, []);
 
+  const handleOnSubmit = (event) => {
+    event.preventDefault();
 
-useEffect(() =>{
-  fetch(API)
-  .then(res => res.json())
-  .then(data => {
-    setMovies(data.results);
-  });
-},[]);
+    fetch(SEARCH_API + searchTerm)
+      .then((res) => res.json())
+      .then((data) => {
+        setMovies(data.results);
+      });
+  };
 
-const handleOnSubmit = (event) =>{
-  event.preventDefault();
-
-  fetch(SEARCH_API + searchTerm)
-  .then(res => res.json())
-  .then(data => {
-    setMovies(data.results);
-  });
-}
-
-const handleOnChange = (e) =>{
-  setSearchTerm(e.target.value);
-};
+  const handleOnChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
 
   return (
     <>
       <header className="header">
-        <h1><GiPopcorn />Movie Night</h1>
+        <h1>
+          <GiPopcorn />
+          Movie Night
+        </h1>
         <form onSubmit={handleOnSubmit}>
-          <input className="header_search" type="search" placeholder="Search..." value={searchTerm} onChange={handleOnChange}/>
+          <input
+            className="header_search"
+            type="search"
+            placeholder="Search..."
+            value={searchTerm}
+            onChange={handleOnChange}
+          />
         </form>
       </header>
       <div className="container">
-        {movies && movies.map((movie) =>(
-          <Movie key={movie.id} {...movie}/>
-        ))}
+        {movies && movies.map((movie) => <Movie key={movie.id} {...movie} />)}
       </div>
     </>
   );
